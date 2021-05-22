e1516f486c23b3290e9470045e4cf21caac73cd9
kunalkale98 <47416520+kunalkale98@users.noreply.github.com>
2021-05-22 11:44:01 +0530

    Create clean-up.sh

---
+++ scripts/clean-up.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /
+if [ -d /new_chatapp/fundoo ]; then
+    sudo rm -rf /new_chatapp/fundoo
+fi
+if [ -d /new_chatapp/requirements.txt ]; then
+    sudo rm -rf /new_chatapp/requirements.txt
+fi
