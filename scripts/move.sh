88c39d7f9ae43b48634f198c17547b7b324e3599
kunalkale98 <47416520+kunalkale98@users.noreply.github.com>
2021-06-10 14:30:23 +0530

    Update move.sh

--- scripts/move.sh
+++ scripts/move.sh
@@ -3,4 +3,6 @@ sudo mv /home/ubuntu/new_chatapp/fundoo /new_chatapp/
 sudo mv /home/ubuntu/new_chatapp/requirements.txt /new_chatapp/
 sudo mv /home/ubuntu/new_chatapp/appspec.yml /new_chatapp/
 sudo mv /home/ubuntu/new_chatapp/scripts /new_chatapp/
+sudo mv /home/ubuntu/new_chatapp/.github /new_chatapp/
+sudo mv /home/ubuntu/new_chatapp/.idea /new_chatapp/
 sudo rm -r /home/ubuntu/new_chatapp
