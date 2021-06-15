0c7325d8ed7a01f8f80f20097afe57608145b3af
kunalkale98 <47416520+kunalkale98@users.noreply.github.com>
2021-06-15 10:43:55 +0530

    Update jenkinsFile.groovy

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -13,6 +13,15 @@ pipeline {
                 }
             }
         }
+        stage ("Quality gate") {
+            steps {
+                timeout(time: 3, unit: 'MINUTES') {
+                    timeout(time: 1, unit: 'HOURS') {
+                        waitForQualityGate abortPipeline: true
+                    }
+                }
+            }
+        }
         stage('Build') {
             steps {
                 sh 'rsync -r -e "ssh -i /home/ubuntu/kunal_123.pem" /var/lib/jenkins/workspace/new_chatapp ubuntu@10.1.1.15:/home/ubuntu/'
