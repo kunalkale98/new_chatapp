372944d3f68a34a7de0f83b7abd8ccb3bf1c435a
kunalkale98 <47416520+kunalkale98@users.noreply.github.com>
2021-06-24 12:13:56 +0530

    Update cleanUp.sh

--- scripts/cleanUp.sh
+++ scripts/cleanUp.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 cd /
-sudo rm -rf /new_chatapp/.github
-sudo rm -rf /new_chatapp/.idea
-sudo rm -rf /new_chatapp/appspec.yml
-sudo rm -rf /new_chatapp/scripts
-if [ -d /new_chatapp/fundoo ];
+sudo rm -rf /home/ubuntu/new_chatapp.github
+sudo rm -rf /home/ubuntu/new_chatapp/.idea
+sudo rm -rf /home/ubuntu/new_chatapp/appspec.yml
+sudo rm -rf /home/ubuntu/new_chatapp/scripts
+sudo rm -rf /home/ubuntu/new_chatapp/Database
+sudo rm -rf /home/ubuntu/new_chatapp/backend
+sudo rm -rf /home/ubuntu/new_chatapp/frontend
+sudo rm -rf /home/ubuntu/new_chatapp/Dockerfile
+sudo rm -rf /home/ubuntu/new_chatapp/docker-compose.yaml
+sudo rm -rf /home/ubuntu/new_chatapp/jenkinsFile.groovy
+if [ -d /home/ubuntu/new_chatapp/fundoo ];
 then
-    sudo rm -rf /new_chatapp/fundoo
+    sudo rm -rf /home/ubuntu/new_chatapp/fundoo
 fi
-if [ -f /new_chatapp/requirements.txt ];
+if [ -f /home/ubuntu/new_chatapp/requirements.txt ];
 then
-    sudo rm -rf /new_chatapp/requirements.txt
+    sudo rm -rf /home/ubuntu/new_chatapp/requirements.txt
 fi
